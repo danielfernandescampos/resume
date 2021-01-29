b54482426949a4cd60483c12c2082ba52259bc4f
danielfernandescampos <daniel_fcampos@hotmail.com>
2021-01-29 16:30:38 -0300

    responsivity complete

---
+++ js/illustrator.js
@@ -0,0 +1,6 @@
+const illustrator = document.getElementById('illustrator');
+const icons = document.querySelectorAll('.icon-hobby')
+illustrator.addEventListener('mouseover', function(){
+    console.log('illustrator')
+    
+})
